52e54c74ef5799ba22d7c73fc29ca02292e804c1
Wiktor Owczarek <tikolu43@gmail.com>
2024-02-28 16:27:14 +0000

    Changed Key "Text" to "Content"

--- export_chats.py
+++ export_chats.py
@@ -56,8 +56,7 @@ for section in sections:
 			chats[chat] = []
 		message["Type"] = message.pop("Media Type")
 		if message["Type"] == "TEXT":
-			message["Text"] = message.get("Content", message.get("Text", None))
-		message.pop("Text", None)
+			message["Text"] = message.pop("Content")
 		message["Date"] = int(datetime.timestamp(datetime.strptime(message.pop("Created", message.pop("Date", None)), "%Y-%m-%d %H:%M:%S %Z")))
 		chats[chat].append(message)
 
